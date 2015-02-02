e060576bebdd5daccecbb2aed809b50632d3a787
Mikko Koho <mikko.koho@csc.fi>
2015-02-02 19:34:04 +0200

    Data harvesting from HSL working

--- harvester.py
+++ harvester.py
@@ -1,10 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date
+import time
 import iso8601
 
 import json, pprint
+import logging
 import requests
 from lxml import etree
 from collections import defaultdict
+
+def daterange(start_date, end_date):
+    '''
+    Taken from http://stackoverflow.com/a/1060330/1816143
+    '''
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
 
 class APIHarvester(object):
     """
@@ -12,31 +22,37 @@ class APIHarvester(object):
     """
   
     HSL_BASE = 'http://www.poikkeusinfo.fi/xml/v2/'
-    
     FMI_BASE = 'http://data.fmi.fi/fmi-apikey/{apikey}/wfs'
-    FMI_PARAMS = {'request': 'getFeature', 'storedquery_id': 'fmi::forecast::hirlam::surface::point::simple', 'place': 'kaisaniemi,helsinki', 'timestep': 60}
+
     FMI_API_FILE = '../fmi_api.txt'
+    FMI_DATA_FILE = '../data_fmi.json'
+    HSL_DATA_FILE = '../data_hsl.json'
+
+    FMI_PARAMS = {'request': 'getFeature', 'storedquery_id': 'fmi::forecast::hirlam::surface::point::simple', 'place': 'kaisaniemi,helsinki', 'timestep': 60}
     FMI_NAMESPACES = {'BsWfs': 'http://xml.fmi.fi/schema/wfs/2.0', 'wfs': "http://www.opengis.net/wfs/2.0"}
     FMI_WANTED_VALUES = ['Temperature', 'WindSpeedMS', 'Precipitation1h']
-    
-    def __init__(self, debug=False):
-        self.debug = debug
-        if self.debug:
-          print('Harvester initialized')
+
+    def __init__(self, loglevel=logging.INFO, logfile='../harvester.log', ):
+        logging.basicConfig(filename=logfile, level=loglevel, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info('Harvester initialized')
           
         with open(self.FMI_API_FILE, 'r') as f:
-          self.fmi_apikey = f.read().replace('\n', '')
-  
+            self.fmi_apikey = f.read().replace('\n', '')
+
+        self.data_fmi = {}
+        self.data_hsl = {}
+
     def hsl_api(self, when):
         """
         Get disruption info from HSL API
+        :param when: datetime
+        :rtype : int
         """
         date_string = "%02d%02d%s%02d%02d" % (when.day, when.month, when.year, when.hour, when.minute)
         
         url = self.HSL_BASE + date_string
     
-        if self.debug:
-          print('Getting disruptions from %s') % url
+        logging.info('Getting disruptions from %s' % url)
     
         result = requests.get(url)
         result_xml = result.text.encode('ascii', 'ignore')
@@ -49,8 +65,7 @@ class APIHarvester(object):
     
         disruptions = result_etree.get('valid')
         
-        if self.debug:
-          print('Got %s disruptions') % disruptions
+        logging.info('Got %s disruptions' % disruptions)
           
         return int(disruptions)
   
@@ -60,8 +75,7 @@ class APIHarvester(object):
         """
         url = self.FMI_BASE.format(apikey=self.fmi_apikey)
         
-        if self.debug:
-          print('Getting forecast from {url} with parameters {params}').format(url=url, params=params)
+        logging.info('Getting forecast from {url} with parameters {params}'.format(url=url, params=params))
     
         result = requests.get(url, params=params)
         result_xml = result.text.encode('ascii', 'ignore')
@@ -80,22 +94,57 @@ class APIHarvester(object):
                 forecasts[time].update({key: value})
             
         #pprint.pprint(forecasts)
-        if self.debug:
-            print 'Received forecasts for {num} time instants'.format(num=len(forecasts))
-    
+        logging.info('Received forecasts for {num} time instants'.format(num=len(forecasts)))
 
-    def test_HSL(self):
-        assert harvester.hsl_api(datetime(2010, 9, 17, 9, 0)) == 2
-        assert harvester.hsl_api(datetime(2011, 9, 17, 9, 0)) == 4
-        assert harvester.hsl_api(datetime(2015, 1, 29, 15, 15)) == 0
+        return forecasts
+
+    def test_hsl(self):
+        assert self.hsl_api(datetime(2010, 9, 17, 9, 0)) == 2
+        assert self.hsl_api(datetime(2011, 9, 17, 9, 0)) == 4
+        assert self.hsl_api(datetime(2015, 1, 29, 15, 15)) == 0
         print 'HSL OK'
-        
 
-        
+    def harvest_all(self, harvest_start, harvest_end, delay=2):
+        """
+        Harvest actual data and save it to json files.
+        """
+        with open(self.FMI_DATA_FILE, 'r') as f:
+            try:
+                self.data_fmi = json.load(f)
+                logging.info('Read %s FMI data objects' % len(self.data_fmi))
+            except ValueError:
+                logging.error('Unable to read %s' % self.FMI_DATA_FILE)
 
-harvester = APIHarvester(debug=True)
+        with open(self.HSL_DATA_FILE, 'r') as f:
+            try:
+                self.data_hsl = json.load(f)
+                logging.info('Read %s HSL data objects' % len(self.data_hsl))
+            except ValueError:
+                logging.error('Unable to read %s' % self.HSL_DATA_FILE)
 
-#harvester.test_HSL()
+        for single_date in daterange(harvest_start, harvest_end):
+            for hour in range(0, 23):
+                instant = datetime(single_date.year, single_date.month, single_date.day, hour)
+                datestr = instant.isoformat()
+                # self.data_fmi.update()
+                if self.data_hsl.get(datestr) is None:
+                    self.data_hsl.update({datestr: self.hsl_api(instant)})
+                    time.sleep(delay)
 
-harvester.fmi_forecast()
+        with open(self.FMI_DATA_FILE, 'w') as f:
+            logging.info('Dumping %s objects of FMI data to %s' % (len(self.data_fmi), self.FMI_DATA_FILE))
+            json.dump(self.data_fmi, f)
+
+        with open(self.HSL_DATA_FILE, 'w') as f:
+            logging.info('Dumping %s objects of HSL data to %s' % (len(self.data_hsl), self.HSL_DATA_FILE))
+            json.dump(self.data_hsl, f)
+
+
+harvester = APIHarvester()
+
+#harvester.test_hsl()
+
+# harvester.fmi_forecast()
+
+harvester.harvest_all(date(2008, 1, 1), date(2008, 12, 31), delay=1)
 
