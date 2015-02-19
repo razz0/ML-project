9b0bc31006e85fdeeecc5b5674f3b29f0d3f09a6
Mikko Koho <mikko.koho@csc.fi>
2015-02-19 17:19:12 +0200

    Add static method to read data file

--- apiharvester.py
+++ apiharvester.py
@@ -62,6 +62,17 @@ class APIHarvester(object):
             with open(self.FMI_API_FILE, 'r') as f:
                 self.fmi_apikey = f.read().replace('\n', '')
 
+    @staticmethod
+    def read_datafile(filename):
+        with open(filename, 'r') as f:
+            try:
+                data = json.load(f)
+                logging.info('Read %s data objects from %s' % (len(data), filename))
+                return data
+            except ValueError:
+                logging.error('Unable to read %s' % filename)
+                return None
+
     def read_fmi_datafile(self):
         with open(self.FMI_DATA_FILE, 'r') as f:
             try:
