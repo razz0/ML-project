84862c1fe179586e62986f22d357a3583a1ced71
Mikko Koho <mikko.koho@csc.fi>
2015-02-22 21:13:16 +0200

    Add models to modules

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 version = '0.0.5'
 
 setup(
-    name='ML-project',
+    name='traffic_disruption',
     version=version,
     author='Mikko Koho',
-    py_modules=['apiharvester'],
+    py_modules=['apiharvester', 'models'],
     install_requires=[
         'lxml >= 3.1.2',
         'iso8601',
